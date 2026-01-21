e5528b71ec961b38fda19a0e1a47dd139fe68ec8
vladimir.petrenko <vladimir.petrenko@jetbrains.com>
2026-01-21 23:29:40 +0100

    add 1 more button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById('clickBtn').addEventListener('click', function() {
+document.getElementById('button1').addEventListener('click', function() {
     alert('buttun clicker');
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    alert('Another button clicked');
 });
