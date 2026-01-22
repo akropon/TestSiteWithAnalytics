acd8739a8b48a606fc9b26cf5608e6d74aaefb40
vladimir.petrenko <vladimir.petrenko@jetbrains.com>
2026-01-22 01:27:58 +0100

    add GTM statistics collection

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('button1').addEventListener('click', function() {
-    alert('buttun clicker');
-});
-
-document.getElementById('button2').addEventListener('click', function() {
-    alert('Another button clicked');
-});
